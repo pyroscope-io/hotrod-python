29ea865c123e5b48dc43eae4be2c543a5add912b
Dmitry Filimonov <me@dfilimonov.com>
2021-01-04 03:25:31 -0800

    bunch of changes

--- Makefile
+++ Makefile
@@ -1,3 +1,16 @@
-.PHONY: docker-hotrod
-docker-hotrod:
-	docker build . -t pyroscope/hotrod-python:dev
+.PHONY: docker-login
+docker-login:
+	aws ecr get-login-password \
+    --region us-east-1 \
+	| docker login \
+    --username AWS \
+    --password-stdin 489437247049.dkr.ecr.us-east-1.amazonaws.com
+
+.PHONY: docker
+docker: docker-login
+	docker build --platform linux/amd64 -t 489437247049.dkr.ecr.us-east-1.amazonaws.com/hotrod-python:dev .
+	docker push 489437247049.dkr.ecr.us-east-1.amazonaws.com/hotrod-python:dev
+
+.PHONY: deploy
+deploy:
+	ssh ec2-user@54.162.85.30 'sh /home/ec2-user/docker-login.sh ; docker-compose down; docker-compose pull; docker-compose up -d'
--- scripts/mock_requests.py
+++ scripts/mock_requests.py
@@ -1,19 +1,18 @@
 import requests
 import time
 import random
+import sys
 import threading
 from queue import Queue
 import multiprocessing
 
-def make_requests_every(time_between_requests, max_requests):
+def make_requests_every(name, time_between_requests, max_requests, num_threads):
     i = 0
     while i < max_requests:
         if i < max_requests / 2:
-            resp_status = make_request(delay=1)
+            resp_status = make_request(name=name, delay=1)
         else:
             threads = []
-            # num_threads = max(10, multiprocessing.cpu_count() * 4)
-            num_threads = 2
 
             r = random.random()
             if r < 0.25:
@@ -22,7 +21,7 @@ def make_requests_every(time_between_requests, max_requests):
                 delay = 2
 
             for j in range(num_threads):
-                t = threading.Thread(target=make_request, args=[delay])
+                t = threading.Thread(target=make_request, args=[name, delay])
                 t.start()
                 threads.append(t)
 
@@ -38,14 +37,17 @@ def make_requests_every(time_between_requests, max_requests):
     print('done')
 
 
-def make_request(delay):
+def make_request(name, delay):
     print(f'making request with delay {delay}')
-    resp = requests.get(f'https://python-hotrod.pyroscope.io/dispatch?customer=392&nonse=0.3238248658061229&mutex_delay={delay}')
+    resp = requests.get(f'https://{name}-hotrod.pyroscope.io/dispatch?customer=392&nonse=0.3238248658061229&mutex_delay={delay}')
     return resp.status_code
 # Python program to use
 # main for function call.
 if __name__ == "__main__":
-    make_requests_every(1, 500)
+    name = sys.argv[1]
+    threads = sys.argv[2]
+    while True:
+        make_requests_every(name, 1, 100, threads)
 
 
 
